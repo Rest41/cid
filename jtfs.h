d91645b71901c9fb56bbddf8bfdd371b060bfb0b
Rest41 <88280807+Rest41@users.noreply.github.com>
2022-06-09 12:30:53 +0700

    Add files via upload

---
+++ jtfs.h
@@ -0,0 +1,44 @@
+#include <cstdlib>
+#include <string>
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <sys/cdefs.h>
+#include <vector>
+#include <time.h>
+#include <cstring>
+#include <unistd.h>
+#include <stdint.h>
+#include "json.hpp"
+#include "packet.h"
+#define byte uint8_t
+#define BYTE uint8_t
+
+//This Is The Customization Area, For Edit
+
+#pragma once
+//Customing GrowID
+string Growid_acc = "tobiz8";
+
+//Customing Password
+string Password_acc = "Nub132$132";
+
+//Customing Gmail ( At least put a random gmail. Dont add @gmail.com )
+string Gmail_acc = "rajjsjajajal";
+
+//Customing Port
+int port = 17257; 
+
+//Customing Start From (Account)
+int START_ACC = 1;
+
+//Customing Start From (Gmail)
+int START_GMAIL = 1;
+
+
+//You can check the port in website http://growtopia2.com/growtopia/server_data.php
+//the current port growtopia is 17198,17246,17249,17251,17253,17256,17258
+//and you can check the created account in file acc.txt
+//if want stop the script click CTRL+C
+//this script has been modified by JTFS GT
+//you can support me by subscribe channel youtube JTFS GT
